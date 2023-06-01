a2d651489b0271b626e2c6779cce95d33a4af4e9
yos-sk <stl1frs2424@gmail.com>
2023-06-01 15:28:03 +0900

    Read name sort

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use rust_htslib::tpool::Error;
 use std::env;
 use std::collections::HashMap;
 
-// TODO: read nameでソートして，read nameが変わったら，出力するというコードに変える必要あり。
 
 #[allow(unused)]
 fn main() {
@@ -150,9 +149,9 @@ fn get_read_position(cigartuples: &Vec<(usize, u32)>) -> (u32, u32) {
 fn get_current_ref_pos(cigartuples: &Vec<(usize, u32)>, ref_start: i64, ref_end: i64, it_start: usize, it_end: usize, strand: String) -> (u32, u32) {
     let mut out_start: u32 = if strand == "+" {
         ref_start as u32
-     } else {
+    } else {
         ref_end as u32
-     } ;
+    } ;
     let mut out_end: u32 = out_start;
     let r_start: u32 = it_start as u32;
     let r_end: u32 = it_end as u32;
@@ -223,7 +222,6 @@ fn cal_count_marker(bamfile: &str, hap1_tabix: &str, hap2_tabix: &str, kmer_size
         .expect(&format!("Could not open {}", bamfile));
     
     let header = bam.header().target_names();
-    // println!("{:?}", header);
     
     let mut headers: HashMap<u32, String> = HashMap::new();
     for name in header {
@@ -231,22 +229,78 @@ fn cal_count_marker(bamfile: &str, hap1_tabix: &str, hap2_tabix: &str, kmer_size
         let r_string = convert_u82String(name);
         headers.insert(r_tid, r_string);
     }
-    //println!("{:?}", headers);
 
 
     let mut sequences: HashMap<String, String> = HashMap::new();
     let mut alignments: HashMap<String, Vec<(String, i64, i64, u32, u32, String, usize, usize, usize)>> = HashMap::new();
-    //let sec_alignments: HashMap<String, Vec<String>> = HashMap::new();
+
+    let mut read_alignments: Vec<bam::record::Record> = Vec::new();
+    let mut prev_read_id = String::new();
 
     let mut line_num = 0;
-    for record in bam.records() {
+    for r_record in bam.records() {
         line_num += 1;
         eprintln!("Processing line {}", line_num);
-        let record = record.unwrap();
+        let record = r_record.unwrap();
         if record.is_unmapped() {
             continue;
         }
         let read_id = convert_u82String(record.qname());
+
+        if prev_read_id.len() == 0 {
+            prev_read_id = read_id;
+            read_alignments.push(record);
+            continue;
+        }
+
+        if read_id != prev_read_id {
+            let t_alignments = process_read_alignments(&read_alignments, &headers, &mut hap1_tbx_reader, &mut hap2_tbx_reader, kmer_size);
+            alignments.insert(prev_read_id, t_alignments);
+            read_alignments = Vec::new();
+            read_alignments.push(record);
+            prev_read_id = read_id;
+        } else {
+            read_alignments.push(record);
+        }
+    }
+    let t_alignments = process_read_alignments(&read_alignments, &headers, &mut hap1_tbx_reader, &mut hap2_tbx_reader, kmer_size);
+    alignments.insert(prev_read_id, t_alignments);
+
+    for (key, value) in alignments.iter_mut() {
+        value.sort_by(|t1, t2| {
+            let key_a = (t1.3 as isize, -(t1.4 as isize));
+            let key_b = (t2.3 as isize, -(t2.4 as isize));
+            key_a.cmp(&key_b)
+        });
+
+        eprint!("{}\t", key);
+        for (i, tuple) in value.iter().enumerate() {
+            if i != value.len() - 1 {
+                eprint!("{},{},{},{},{},{},{},{},{}\t", tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5, tuple.6, tuple.7, tuple.8);
+            } else {
+                eprintln!("{},{},{},{},{},{},{},{},{}", tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5, tuple.6, tuple.7, tuple.8);
+            }
+        }
+    }
+
+    alignments
+}
+
+fn process_read_alignments(read_alignments: &Vec<bam::record::Record>, headers:&HashMap<u32, String>, hap1_tbx_reader: &mut tbx::Reader, hap2_tbx_reader: &mut tbx::Reader, kmer_size: u32) 
+-> Vec<(String, i64, i64, u32, u32, String, usize, usize, usize)>
+{
+    let mut records = read_alignments.clone();
+    records.sort_by_key(|t| {
+        t.flags()
+    });
+
+    let mut counted_alignments: Vec<(String, i64, i64, u32, u32, String, usize, usize, usize)> = Vec::new();
+    let mut sequence = String::new();
+    for (i, record) in records.iter().enumerate() {
+        if i == 0 {
+            sequence = convert_u82String(&record.seq().as_bytes());
+            // eprintln!("{}, {}, {}", read_id, record.flags(), sequence.len());
+        }
         let reference_id = record.tid() as u32;
         let reference_name = headers.get(&reference_id).unwrap(); 
         let ref_start = record.pos();
@@ -259,32 +313,23 @@ fn cal_count_marker(bamfile: &str, hap1_tabix: &str, hap2_tabix: &str, kmer_size
         let mut read_seq = String::new();
         if record.is_reverse() {
             if record.is_supplementary() | record.is_secondary() {
-                if let Some(value) = sequences.get(&read_id) {
-                    read_seq = reverse_complement(&value.to_owned());
+                if sequence.len() != 0 {
+                    read_seq = reverse_complement(&sequence);
                 } else {
                     eprintln!("Bad alignment: BamFile needs to be sorted");
                 }
             } else {
-                read_seq = reverse_complement(&convert_u82String(&record.seq().as_bytes()));
+                read_seq = reverse_complement(&sequence);
             }
         } else {
             if record.is_supplementary() | record.is_secondary() {
-                if let Some(value) = sequences.get(&read_id) {
-                    read_seq = value.to_owned();
+                if sequence.len() != 0 {
+                    read_seq = sequence.clone();
                 } else {
                     eprintln!("Bad alignment: BamFile needs to be sorted");
                 }
             } else {
-                read_seq = convert_u82String(&record.seq().as_bytes());
-            }
-        }
-
-        if !record.is_secondary() & !record.is_supplementary() {
-            if record.is_reverse() {
-                let reverse_sequence = reverse_complement(&read_seq);
-                sequences.insert(read_id.clone(), reverse_sequence.clone());
-            } else {
-                sequences.insert(read_id.clone(), read_seq.clone());
+                read_seq = sequence.clone();
             }
         }
         
@@ -300,26 +345,24 @@ fn cal_count_marker(bamfile: &str, hap1_tabix: &str, hap2_tabix: &str, kmer_size
             0
         };
         
-        // let read_length = read_seq.len();
-        // println!("{:?}", read_seq);
+
         let cigartuples = get_cigartuples(&record);
         let read_pos: (u32, u32) = get_read_position(&cigartuples);
         let read_start = read_pos.0;
         let read_end = read_pos.1;
-        // eprintln!("{} {}", read_start, read_end);
         let read_length = read_end - read_start;
 
         let mut kmer_cnt: usize = 0;
 
         let delimiter: u8 = 9; // '\t' for ASCII code
-        let mut tbx_reader = &mut hap1_tbx_reader;
+        let mut tbx_reader = &mut *hap1_tbx_reader;
         if &reference_name[0..2] == "h1" {
             let tid = match tbx_reader.tid(reference_name) {
                 Ok(tid) => tid,
                 Err(_) => {
                     let info: (String, i64, i64, u32, u32, String, usize, usize, usize)
                         = (reference_name.to_string(), ref_start, ref_end, read_start, read_end, read_strand.to_string(), is_sec, is_supp, kmer_cnt);
-                    alignments.entry(read_id).or_insert(Vec::new()).push(info);
+                    counted_alignments.push(info);
                     continue;
                 },
             };
@@ -332,18 +375,18 @@ fn cal_count_marker(bamfile: &str, hap1_tabix: &str, hap2_tabix: &str, kmer_size
                 Err(_) => {
                     let info: (String, i64, i64, u32, u32, String, usize, usize, usize) 
                         = (reference_name.to_string(), ref_start, ref_end, read_start, read_end, read_strand.to_string(), is_sec, is_supp, kmer_cnt);
-                    alignments.entry(read_id).or_insert(Vec::new()).push(info);
+                    counted_alignments.push(info);
                     continue;
                 }
             }
         } else {
-            tbx_reader = &mut hap2_tbx_reader;
+            tbx_reader = hap2_tbx_reader;
             let tid = match tbx_reader.tid(reference_name) {
                 Ok(tid) => tid,
                 Err(_) => {
                     let info: (String, i64, i64, u32, u32, String, usize, usize, usize) 
                         = (reference_name.to_string(), ref_start, ref_end, read_start, read_end, read_strand.to_string(), is_sec, is_supp, kmer_cnt);
-                    alignments.entry(read_id).or_insert(Vec::new()).push(info);
+                    counted_alignments.push(info);
                     continue;
                 },
             };
@@ -357,7 +400,7 @@ fn cal_count_marker(bamfile: &str, hap1_tabix: &str, hap2_tabix: &str, kmer_size
                     let info: (String, i64, i64, u32, u32, String, usize, usize, usize)
                         = (reference_name.to_string(), ref_start, ref_end, read_start, read_end, read_strand.to_string(), is_sec, is_supp, kmer_cnt);
                     // println!("{} {}", read_id, kmer_cnt);
-                    alignments.entry(read_id).or_insert(Vec::new()).push(info);
+                    counted_alignments.push(info);
                     continue;
                 }
             }
@@ -392,7 +435,6 @@ fn cal_count_marker(bamfile: &str, hap1_tabix: &str, hap2_tabix: &str, kmer_size
         let it_start: usize = read_start.try_into().unwrap();
         let it_end: usize = read_end.try_into().unwrap();
         let k: usize = kmer_size.try_into().unwrap();
-        // eprintln!("Iterator: {} {}", it_start, it_end);
         for i in it_start..(it_end-k+1) {
             let slice = (&read_seq[i..(i+k)]).to_string();
             // eprintln!("{}\t{}\t{:?}", slice, seq, slice==seq);
@@ -419,25 +461,9 @@ fn cal_count_marker(bamfile: &str, hap1_tabix: &str, hap2_tabix: &str, kmer_size
 
         let info: (String, i64, i64, u32, u32, String, usize, usize, usize) 
             = (reference_name.to_string(), ref_start, ref_end, read_start, read_end, read_strand.to_string(), is_sec, is_supp, kmer_cnt);
-        alignments.entry(read_id).or_insert(Vec::new()).push(info);
+        counted_alignments.push(info);
     }
-    for (key, value) in alignments.iter_mut() {
-        value.sort_by(|t1, t2| {
-            let key_a = (t1.3 as isize, -(t1.4 as isize));
-            let key_b = (t2.3 as isize, -(t2.4 as isize));
-            key_a.cmp(&key_b)
-        });
-
-        eprint!("{}\t", key);
-        for (i, tuple) in value.iter().enumerate() {
-            if i != value.len() - 1 {
-                eprint!("{},{},{},{},{},{},{},{},{}\t", tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5, tuple.6, tuple.7, tuple.8);
-            } else {
-                eprintln!("{},{},{},{},{},{},{},{},{}", tuple.0, tuple.1, tuple.2, tuple.3, tuple.4, tuple.5, tuple.6, tuple.7, tuple.8);
-            }
-        }
-    }
-    alignments
+    counted_alignments
 }
 
 fn reverse_complement(sequence: &str) -> String {
@@ -634,4 +660,3 @@ fn write_bam(bamfile: &str, output_bam: &str, filtered_alignments: &mut HashMap<
         }
     }
 }
-
