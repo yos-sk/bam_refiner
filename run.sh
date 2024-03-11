c3f0ff5eff8531a3643ff022defaf596e4cdf445
yos-sk <stl1frs2424@gmail.com>
2024-03-11 12:27:16 +0900

    typo

--- run.sh
+++ run.sh
@@ -54,7 +54,7 @@ tabix -p bed ${OUTPUT_DIR}/hap1_cnt_kmerposition.bed.gz
 bam_refiner locate-kmers \
     -i ${WORK_DIR}/meryl/hap2.cnt.uniq.tsv.gz \
     -f ${hap1_contig} \
-    -k 21 | sort -k 1,1 -k 2,2n > ${OUTPUT_DIR}/hap1_cnt_kmerposition.bed
+    -k 21 | sort -k 1,1 -k 2,2n > ${OUTPUT_DIR}/hap2_cnt_kmerposition.bed
 bgzip -f ${OUTPUT_DIR}/hap2_cnt_kmerposition.bed
 tabix -p bed ${OUTPUT_DIR}/hap2_cnt_kmerposition.bed.gz
 
