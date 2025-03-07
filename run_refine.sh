5f60172f8dafb4c7181d71466695e5da35d88098
yos-sk <stl1frs2424@gmail.com>
2025-03-07 18:21:53 +0900

    Modify run_refine.sh

--- run_refine.sh
+++ run_refine.sh
@@ -5,13 +5,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-while getopts "b:df:h:i:o:ps:t:u:" opt; do
+while getopts "b:df:h:i:m:o:ps:t:u:" opt; do
   case $opt in
     b) INPUT_BAM=$OPTARG ;;
     d) DEBUG="true" ;;
     f) INPUT_FASTQ=$OPTARG ;;
     h) HAP1_CONTIG=$OPTARG ;;
     i) HAP2_CONTIG=$OPTARG ;;
+    m) MINIMAP_OPTION=$OPTARG ;;
     o) OUTPUT_DIR=$OPTARG ;;
     p) OPTION_SPLIT="true" ;;
     s) SAMPLE=$OPTARG ;;
@@ -34,6 +35,11 @@ if [ -z "${OUTPUT_DIR:-}" ]; then
     echo "Output directory is not given. Please set -o {OUTPUT_DIR}"; exit 1
 fi
 
+if [ -z "${MINIMAP_OPTION:-}" ]; then
+    MINIMAP_OPTION=""
+    echo "Not given any options for minimap2"
+fi
+
 if [ -z "${OPTION_SPLIT:-}" ]; then
     echo "Split option is not given. Bam_refiner will be performed without splitting a BAM file"
     OPTION_SPLIT="false"
@@ -53,15 +59,15 @@ cat ${HAP1_CONTIG} ${HAP2_CONTIG} > ${WORK_DIR}/reference.fa
 
 if [ -z ${INPUT_BAM:-} ] && [ ! -z ${INPUT_FASTQ:-} ]; then
     if [ ${DATA} == "hifi" ]; then
-        minimap2 -t ${THREAD} -ax asm5 ${WORK_DIR}/reference.fa ${INPUT_FASTQ} | samtools view -Shb > ${OUTPUT_BAM_PREFIX}.unsorted
+        minimap2 -t ${THREAD} -ax asm5 ${MINIMAP_OPTION} ${WORK_DIR}/reference.fa ${INPUT_FASTQ} | samtools view -Shb > ${OUTPUT_BAM_PREFIX}.unsorted
     else
-        minimap2 -t ${THREAD} -ax asm10 ${WORK_DIR}/reference.fa ${INPUT_FASTQ} | samtools view -Shb > ${OUTPUT_BAM_PREFIX}.unsorted
+        minimap2 -t ${THREAD} -ax asm10 ${MINIMAP_OPTION} ${WORK_DIR}/reference.fa ${INPUT_FASTQ} | samtools view -Shb > ${OUTPUT_BAM_PREFIX}.unsorted
     fi
 elif [ ! -z ${INPUT_BAM:-} ]; then
     if [ ${DATA} == "hifi" ]; then
-        samtools fastq -@ 16 -TMM,ML ${INPUT_BAM} | minimap2 -t 16 -ax asm5 -y ${WORK_DIR}/reference.fa - | samtools view -@ 16 -Shb - > ${OUTPUT_BAM_PREFIX}.unsorted
+        samtools fastq -@ 16 -TMM,ML ${INPUT_BAM} | minimap2 -t 16 -ax asm5 -y ${MINIMAP_OPTION} ${WORK_DIR}/reference.fa - | samtools view -@ 16 -Shb - > ${OUTPUT_BAM_PREFIX}.unsorted
     else
-        samtools fastq -@ 16 -TMM,ML ${INPUT_BAM} | minimap2 -t 16 -ax asm10 -y ${WORK_DIR}/reference.fa - | samtools view -@ 16 -Shb - > ${OUTPUT_BAM_PREFIX}.unsorted
+        samtools fastq -@ 16 -TMM,ML ${INPUT_BAM} | minimap2 -t 16 -ax asm10 -y ${MINIMAP_OPTION} ${WORK_DIR}/reference.fa - | samtools view -@ 16 -Shb - > ${OUTPUT_BAM_PREFIX}.unsorted
     fi
 fi
  
